28e14bc2e1840a88915c62c836d7a0ee402289b0
Tulsi Leathers <info@tulsileathers.net>
2018-09-18 13:00:26 -0400

    Change cms base branch

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,15 +1,15 @@
-import CMS from 'netlify-cms'
+import CMS from 'netlify-cms';
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import MusicPagePreview from './preview-templates/MusicPagePreview'
-import ConnectPagePreview from './preview-templates/ConnectPagePreview'
-import HealingSessionsPagePreview from './preview-templates/HealingSessionsPagePreview'
+import AboutPagePreview from './preview-templates/AboutPagePreview';
+import BlogPostPreview from './preview-templates/BlogPostPreview';
+import ProductPagePreview from './preview-templates/ProductPagePreview';
+import MusicPagePreview from './preview-templates/MusicPagePreview';
+import ConnectPagePreview from './preview-templates/ConnectPagePreview';
+import HealingSessionsPagePreview from './preview-templates/HealingSessionsPagePreview';
 
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate('music', MusicPagePreview)
-CMS.registerPreviewTemplate('connect', ConnectPagePreview)
-CMS.registerPreviewTemplate('healing-sessions', HealingSessionsPreview)
+CMS.registerPreviewTemplate('about', AboutPagePreview);
+CMS.registerPreviewTemplate('products', ProductPagePreview);
+CMS.registerPreviewTemplate('blog', BlogPostPreview);
+CMS.registerPreviewTemplate('music', MusicPagePreview);
+CMS.registerPreviewTemplate('connect', ConnectPagePreview);
+CMS.registerPreviewTemplate('healing-sessions', HealingSessionsPagePreview);
