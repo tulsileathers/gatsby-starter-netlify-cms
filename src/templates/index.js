ddb6128bf16b38fd8dd006482bf1f4fb75cbbb3b
Tulsi Leathers <info@tulsileathers.net>
2018-09-25 17:02:34 -0400

    Update instagram and remove filler image from homepage

--- src/templates/index.js
+++ src/templates/index.js
@@ -17,13 +17,13 @@ console.log(data.images)
                     <div className="column is-10 is-offset-1">
                         <div
                             className="full-width-image-container margin-top-0"
-                            style={{ backgroundImage: `url(${post.frontmatter.bannerImage})` }}
+                            /* style={{ backgroundImage: `url(${post.frontmatter.bannerImage})` }} */
                         >
                             <h2
                                 className="has-text-weight-bold is-size-1"
                                 style={{
-                                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                                    backgroundColor: '#f40',
+                                    boxShadow: '0.5rem 0 0 #11c5fc, -0.5rem 0 0 #11c5fc',
+                                    backgroundColor: '#11c5fc',
                                     color: 'white',
                                     padding: '1rem',
                                 }}
